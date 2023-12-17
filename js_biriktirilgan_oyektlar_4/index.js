a1537e702f0141ccfb4b9842617f58ee3911cd78
MuzaffarRaximov <javascriptmernstack@gmail.com>
2023-12-17 22:02:54 +0500

    yangilanish

---
+++ js_biriktirilgan_oyektlar_4/index.js
@@ -0,0 +1,70 @@
+
+//-----------son(Number)------------------
+new Number() // obyekt sifatida aniqlanadi
+
+// Numberni xosalai
+Number.EPSILON
+Number.MAX_SAFE_INTEGER
+Number.MAX_VALUE
+Number.MIN_SAFE_INTEGER
+Number.MIN_VALUE
+Number.NaN
+Number.NEGATIVE_INFINITY
+Number.POSITIVE_INFINITY
+
+//Numberni obyektini functionlari
+Number.isNaN()
+Number.isFinite()
+Number.isInteger()
+Number.isSafeInteger()
+Number.parseFloat()
+Number.parseInt()
+Number.call()
+
+// Number obyectini biriktirilgan funsiyalari
+
+let num =100;
+
+num.toExponential()
+num.toFixed()
+num.toLocaleString()
+num.toPrecision()
+num.toString()
+num.valueOf()
+
+
+//----------Satr( String )------------------------------------
+
+const str = new String('dasturlash');
+
+String.fromCharCode()
+str.charAt()
+str.charCodeAt()
+str.concat()
+str.includes()
+str.endsWith()
+str.startsWith()
+str.indexOf()
+str.lastIndexOf()
+str.localeCompare()
+str.padEnd()
+str.padStart()
+str.repeat()
+str.replace()
+str.slice()
+str.split()
+str.substring()
+str.toLowerCase()
+str.toUpperCase()
+str.toString()
+str.trim()
+str.valueOf()
+
+
+
+
+
+
+
+
+
