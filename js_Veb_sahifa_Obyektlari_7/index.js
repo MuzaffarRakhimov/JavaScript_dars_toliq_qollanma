6daa3bfb3b87189fc16da26128ea47e208b967c1
MuzaffarRaximov <javascriptmernstack@gmail.com>
2023-12-19 22:09:40 +0500

    sahifalar

---
+++ js_Veb_sahifa_Obyektlari_7/index.js
@@ -0,0 +1,385 @@
+
+//-----------------Window obyekti-----------------------------------
+
+
+window.crypto
+window.devicePixelRatio
+window.frameElement
+window.frames
+window.innerHeight
+window.innerWidth
+window.name
+window.isSecureContext
+window.length
+window.location
+window.locationbar
+window.localStorage
+window.menubar
+window.opener
+window.outerHeight
+window.outerWidth
+window.parent
+window.personalbar
+window.screen
+window.scrollbars
+window.scrollX
+window.self
+window.sessionStorage
+window.statusbar
+window.toolbar
+window.top
+window.window
+
+//--------------Windowni uslublari-----------------------
+
+window.alert()
+window.btoa()
+window.atob()
+window.open()
+window.close()
+window.stop()
+window.setInterval()
+window.clearInterval()
+window.setTimeout()
+window.clearTimeout()
+window.focus()
+window.blur()
+window.getComputedStyle()
+window.getSelection()
+window.print()
+window.requestIdleCallback()
+window.cancelIdleCallback()
+window.scroll()
+window.scrollBy()
+window.scrollTo()
+
+
+//-----------Windowni Hodisalari ----------------------------------
+
+window.onafterprint()
+window.onbeforeprint()
+window.onunload()
+window.onbeforeinput()
+window.onunload()
+window.onbeforeunload()
+window.onblur()
+window.onchange()
+window.onclick()
+window.ondblclick()
+window.onclose()
+window.oncontextmenu()
+window.onfocus()
+window.ondeviceorientation()
+window.onerror()
+window.onhashchange()
+window.onmessage()
+window.onoffline()
+window.ononline()
+window.onresize()
+window.addEventListener()
+
+//-------------------Sichqoncha harakatiga oid------------------------
+
+altKey
+ctrlKey
+shiftKey
+
+button:[
+    0-'asosiy(chap)tugma',
+    1-'qoshimcha (surgich)tugmasi',
+    2-'ikkinchi(ong) tugma',
+    3-'avvalgi sahifaga otkazuvchi tugma',
+    4-'keyingi sahifaga otkazuvchi tugma'
+]
+screenX
+screenY
+clientX
+clientY
+movementX
+movementY
+offsetX
+offsetY
+pageX
+pageY
+
+
+//-----------location obyecti---------------------------------------
+
+location.href
+location.protocol
+location.host
+location.hostname
+location.port
+location.pathname
+location.search
+location.hash
+location.origin
+
+
+//-------------------history obyekti-----------------------------
+
+history.back()
+history.forward()
+history.go()
+history.pushState()
+history.replaceState()
+history.state
+
+//--------------navigator obyekti--------------------
+
+navigator.cookieEnabled
+navigator.doNotTrack
+navigator.geolocation
+navigator.onLine
+navigator.language
+navigator.languages
+navigator.userAgent
+navigator.serviceWorker
+navigator.registerProtocolHandler()
+navigator.vibrate()
+
+
+//---------------------performance obyecti------------------------
+
+performance.setResourceTimingBufferSize()
+performance.onresourcetimingbufferfull()
+performance.navigator
+performance.now()
+performance.mark()
+performance.getEntriesByName()
+performance.clearMeasures()
+performance.clearMarks()
+performance.measure()
+performance.getEntriesByType()
+performance.getEntries()
+
+//-------------------document obyekti-------------------
+ 
+document
+
+//-------------------Element obyekti ---------------
+
+Element
+        attributes
+        childElementCount
+        children
+        className
+        clientHeight
+        clientWidth
+        firstElementChild
+        lastElementChild
+        innerHTML
+        innerText
+        previousElementSibling
+        id
+        nextElementSibling
+        outerHTML
+        outerText
+        scrollHeight
+        scrollWidth
+        tagName
+        style
+        addEventListener()
+        removeEventListener()
+        closest()
+        getAttributeNames()
+        getAttribute()
+        getAttributeNode()
+        getBoundingClientRect()
+        getClientRects()
+        getElementsByClassName()
+        getElementsByTagName()
+        hasAttribute()
+        insertAdjacentHTML()
+        matches()
+        querySelector()
+        querySelectorAll()
+        remove()
+        removeChild()
+        appendChild()
+        requestFullscreen()
+        requestPointerLock()
+        scrollIntoView()
+        setAttribute()
+        setAttributeNode()
+        removeAttribute()
+        setPointerCapture()
+        releasePointerCapture()
+
+//------------documentni xususiyatlari-----------------------
+
+document.characterSet
+document.compatMode
+document.contentType
+document.doctype
+document.documentElement
+document.documentURI
+document.activeElement
+document.body
+document.defaultView
+document.designMode
+document.dir
+document.embeds
+document.forms
+document.head
+document.images
+document.lastModified
+document.links
+document.plugins
+document.readyState
+document.referrer
+document.scripts
+document.title
+document.URL
+
+
+//--------------documentni uslublari-----------------------------
+
+document.adoptNode()
+document.createAttribute()
+document.createAttribute()
+document.createComment()
+document.createElement()
+document.createDocumentFragment()
+document.createNodeIterator()
+document.createTreeWalker()
+document.createRange()
+document.createTextNode()
+document.elementFromPoint()
+document.exitPointerLock()
+document.importNode()
+document.getSelection()
+document.hasFocus()
+document.open()
+document.write()
+document.close()
+
+//-----------------document cookie---------------------------------------
+
+document.cookie
+
+//--------------Ajax orqali malumot almashish------------------------------
+
+fetch()
+method 
+headers 
+body
+mode 
+credentials 
+cache 
+redirect
+referrer
+referrerPolicy
+integrity
+keepalive
+signal 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
