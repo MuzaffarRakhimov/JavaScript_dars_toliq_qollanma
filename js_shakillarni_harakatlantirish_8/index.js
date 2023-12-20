a5a8c17f6aeb5ede9ac5e11ebc5a71d0e759fd0e
MuzaffarRaximov <javascriptmernstack@gmail.com>
2023-12-20 07:37:52 +0500

    canvas

---
+++ js_shakillarni_harakatlantirish_8/index.js
@@ -0,0 +1,23 @@
+
+//--------------Canvas obyekti ------------------------------------
+
+let k = new Path2D()
+
+k.moveTo()
+k.lineTo()
+k.bezierCurveTo()
+k.arc()
+k.arcTo()
+k.ellipse()
+k.rect()
+k.closePath()
+
+
+//---------------------Suzuvchi oynalar ---------------------------------------
+
+
+
+
+
+
+
--- js_takrorlash_operatori/index.js
+++
@@ -1,5 +0,0 @@
-// for loop operatori
-let b =20;
-for (let i = 0; i <b ; i++) {
-    console.log(i);
-}
